e7bf05f1a360f5f4e5f0dc93a2a18776f000601f
Giulia <giulia.pennati94@gmail.com>
2017-11-23 20:19:43 +0100

    dinamizzando la pagina

---
+++ GEA/PHP/config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * GEA - gioco educazione alimentare
+ * Configuration file
+ * Author: Pennati Giulia, Blanco Federica
+ */
+
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'gea');
+define('DB_PASSWORD','sarkefanti35');
+define('DB_NAME', 'my_gea');
+
+?>
---
+++ GEA/Script/primaryScript.js
@@ -0,0 +1,12 @@
+$(function primary() {
+// default behaviour: load homepage
+	$(".contenitore").load( "home.html");/*, function() {
+		
+		// after loading the page we should load the page manager for links
+		$.getScript('script/pageManager.js', function() {
+			// load dynamic links (index.html)
+			clickPageLinks();
+		});
+	});	*/
+    
+});
