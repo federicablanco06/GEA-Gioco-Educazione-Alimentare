bca110f67d4e5312bd855de47296067e946528c3
Giulia <giulia.pennati94@gmail.com>
2017-11-25 17:56:38 +0100

    scripting

--- GEA/Script/primaryScript.js
+++ GEA/Script/primaryScript.js
@@ -10,3 +10,12 @@ $(function primary() {
 	});	*/
     
 });
+
+/*$(function difficulty(int game) {
+    if (game==1)
+        $(".contenitore").load("diff_piramide.html");
+    else if(game==2)
+        $(".contenitore").load("diff_sano.html");
+    else if(game==3)
+        $(".contenitore").load("diff_tavola.html");
+});*/
