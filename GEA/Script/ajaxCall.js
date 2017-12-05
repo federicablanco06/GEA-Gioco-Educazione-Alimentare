ac6b1dbf75f2283a392a8a4ce802e40624ac71b9
federicablanco <blue_angel94@hotmail.it>
2017-12-05 17:18:41 +0100

    ajax funzionante..unico problema percorso immagini

--- GEA/Script/ajaxCall.js
+++ GEA/Script/ajaxCall.js
@@ -12,53 +12,63 @@ function getSano(difficulty) {
          
         //estrazione elementi chiamati dalla query del database
         url: "http://gea.altervista.org/PHP/getSano.php", //Relative or absolute path to file.php file
-        data: {id:diff},
+        data: {id: diff},
          
         //trasmissione dati riuscita
-        success: function(response) {
-            var json=JSON.parse(response);
+        success: function (response) {
+            var json = JSON.parse(response);
             var content = '';
-            $.each(json[0], function(key, value){
+            $.each(json[0], function (key, value) {
                 content += value;
             });
             
             
             //in base alla difficoltà richiesta, avrò estrazioni differenti all'array json            
-            //smistamento in base a difficoltà
-            var difficultyArray=[];
-            for(var i=0; i<json.length; i++) {
-                if(json[i].diff==difficulty) 
-                    difficultyArray.push(json[i]);
+            //smistamento in base a difficoltà e correttezza
+            var corrdifficultyArray = [];
+            var sbadifficultyArray = [];
+            for (var i = 0; i<json.length; i++) {
+                if(json[i].diff == difficulty && json[i].corr == '1') 
+                    corrdifficultyArray.push(json[i]);
+                else{
+                    if(json[i].diff == difficulty && json[i].corr == '0')
+                        sbadifficultyArray.push(json[i]);
+                }
             }
             
-            //smistamento in base a correttezza
-            var corretto=[];
-            var sbagliato=[];
             
-            console.log("elemento 0 " + difficultyArray[0]);
-            console.log("\n correttezza " + difficultyArray[0].corr);
-            
-            //C'E' UN ERRORE QUI NON VENGONO RIEMPITI GLI ARRAY
-            for(var j=0; j<difficultyArray.lenght; j++) {
-                if(difficultyArray[i].corr==1)
-                    corretto.push(difficultyArray[i]);
-                else
-                    sbagliato.push(difficultyArray[i]);
-            }
-            console.log("array1 " + corretto);
-            console.log("array2 " + sbagliato);
-            
+          
+            console.log("elemento 0 " + corrdifficultyArray[0].img + sbadifficultyArray[0].img);
+            console.log("\n correttezza " + corrdifficultyArray[0].corr + sbadifficultyArray[0].corr);
+        
+                        
             //scelgo random le due immagini
-            var rdm1=  Math.floor(Math.random() * (corretto.lenght)) + 1;
-            var rdm2=  Math.floor(Math.random() * (sbagliato.lenght)) + 1;
+            function getRandomInt(min, max) {
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+            var rand1= getRandomInt(0,corrdifficultyArray.length-1);
+            var rand2= getRandomInt(0,sbadifficultyArray.length-1);
+                
             
-            var risultati=[];
-            risultati.push(corretto[rdm1]);
-            risultati.push(sbagliato[rdm2]);
+            console.log(rand1);
+            console.log(rand2);
             
+            //var risultati=[];
+            var corr = corrdifficultyArray[rand1];
+            var sba = sbadifficultyArray[rand2];
+            
+            console.log(corr.img);
+            console.log(sba.img);
+            
+            var elm1= corr.img;
+            var elm2= sba.img;
+
+            //variabili di gioco
+            $('#table').after('<a-image class="sano" position="4.7 1.5 4.5" material="src:'+ elm1 +'" scale="0.7 0.7 0.7"></a-image>');
+            $('#table').after('<a-image class="sano" position="5.9 1.5 4.5" material="src:'+ elm2 +'" scale="0.7 0.7 0.7"></a-image>');
             
             //e infine le mando al game master
-            return risultati;
+            return [corr, sba];
             
             
         },
