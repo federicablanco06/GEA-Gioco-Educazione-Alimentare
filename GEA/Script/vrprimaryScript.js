1f8e8b56966b4191741001f01bf54210a5a8a83a
Giulia <giulia.pennati94@gmail.com>
2018-01-22 11:41:38 +0100

    aggiunto camera-fix

--- GEA/Script/vrprimaryScript.js
+++ GEA/Script/vrprimaryScript.js
@@ -33,3 +33,21 @@ $(function vrprimary() {
     });
     
 });
+
+AFRAME.registerComponent('camera-fix', {
+    schema: {
+    },
+
+    init: function () {
+        if(AFRAME.utils.device.isMobile()) {
+        this.onEnterVR = AFRAME.utils.bind(this.onEnterVR, this);
+        this.el.sceneEl.addEventListener('enter-vr', this.onEnterVR);
+        }
+    },
+    //fix camera height that sums 1.6 every time we enter in VR mode
+
+    onEnterVR: function () {
+    this.el.setAttribute("position", "0 0 0");
+    }
+
+});
