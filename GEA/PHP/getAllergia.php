08f8d646b50e837aadc98aede7198c29e63203d5
federicablanco <blue_angel94@hotmail.it>
2018-11-17 15:11:49 +0100

    php allergia

---
+++ GEA/PHP/getAllergia.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * GEA - AUI project 2017-2018
+ * getPiramide.php
+ * Query for "Riconosci l'allergene!" images 
+ * Authors: Pennati Giulia, Blanco Federica
+ */
+
+header('Access-Control-Allow-Origin: *');
+
+require_once 'config.php';
+
+//connection to db
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if (mysqli_connect_errno()) { //verify connection
+    echo "Error to connect to DBMS: ".mysqli_connect_error(); //notify error
+    exit(); //do nothing else
+}
+else {
+    //echo "Successful connection"; // connection ok
+
+    //# extract results mysqli_result::fetch_array
+    $query = "SELECT id, img, diff, sedano, molluschi, uova, pesce, latticini, guscio, crostacei, soia, noci, glutine FROM Allergia";
+    //query execution
+    $result = $mysqli->query($query);
+
+    //if there are data available
+    if($result->num_rows >0)
+    {
+        $myArray = array();//create an array
+        while($row = $result->fetch_array(MYSQL_ASSOC)) {
+
+            $myArray[] = array_map('utf8_encode', $row);
+        }
+        echo json_encode($myArray);
+    }
+	else {
+		echo "La tabella è vuota!";
+	}
+
+    //free result
+    $result->close();
+
+    //close connection
+    $mysqli->close();
+
+
+
+}
+
+
+
+
+
+
+?>
